f90e363f122eb5e29dae4bd2a3f6600375a816dc
Jairo Daniel Bermudez Luengas <jairob92@gmail.com>
2025-11-20 19:45:37 -0500

    Progress: setting HomePage with locators and methods (#1)

---
+++ pages/homePage.ts
@@ -0,0 +1,34 @@
+import { Locator, Page } from "@playwright/test";
+
+export class HomePage{
+
+    private readonly searchInput:Locator
+    private readonly searchButton:Locator
+    private readonly sortSelect:Locator
+    private readonly handToolsCheckbox:Locator
+    private readonly hammerToolCheckbox:Locator
+
+    constructor(page:Page){
+        this.searchInput = page.getByTestId('search-query')
+        this.searchButton= page.getByTestId('search-submit')
+        this.sortSelect = page.getByTestId('sort')
+        this.handToolsCheckbox= page.getByLabel(' Hand Tools')
+        this.hammerToolCheckbox= page.getByLabel(' Hammer')
+    }
+
+    async typeProduct(toolName:string){
+        await this.searchInput.fill(toolName)
+    }
+    async clickOnSearchButton(){
+        await this.searchButton.click()
+    }
+    async selectSort(value:string){
+        await this.sortSelect.selectOption(value)
+    }
+
+
+
+
+
+
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    testIdAttribute:'data-test',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
@@ -39,7 +40,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+   /* {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -47,7 +48,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
